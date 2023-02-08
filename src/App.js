db6710dae3ada4346a7d684389d1c805644e056a
ymkim <wingkym@nate.com>
2023-02-09 03:57:56 +0900

    blog_react

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test11
+          Learn React test1111
         </a>
       </header>
     </div>
